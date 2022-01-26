12732b7e4aa996fc4beca24a5224cb79a9adedf8
GJCode <golla.programmazione@gmail.com>
2022-01-26 08:12:19 +0000

    fix-#3

--- src/app/services/api-mock.service.ts
+++ src/app/services/api-mock.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable} from 'rxjs';
+import { Observable, of} from 'rxjs';
+import { HTTPBasicResponse } from '../interfaces/http-responses/http-basic-response';
+import { Voto } from '../interfaces/punteggi/voto';
 import { ApiServiceService } from './api-service.service';
 
 @Injectable({
@@ -7,14 +9,25 @@ import { ApiServiceService } from './api-service.service';
 })
 export class ApiMockService implements ApiServiceService {
 
+  baseDatiVoti: Voto[] = [];
+
   constructor() { }
   
-  sendVoti(voti: any[]) {
-    throw new Error('Method sendVoti Mock not implemented.');
+  sendVoti(voti: Voto[]): Observable<HTTPBasicResponse> {
+    for (let i = 0; i < voti.length; i++) {
+      let nuovoVoto: Voto = voti[i];
+      let votoDbIndex = this.baseDatiVoti.findIndex((voto:Voto) => voto.name === nuovoVoto.name && voto.game === nuovoVoto.game);
+      if (votoDbIndex !== -1) {
+        this.baseDatiVoti[votoDbIndex].score += nuovoVoto.score;
+      } else {
+        this.baseDatiVoti.push(nuovoVoto);
+      }
+    }
+    return of({status: 200});
   }
 
-  getVoti(): Observable<any[]> {
-    throw new Error('Method getVoti Mock not implemented.');
+  getVoti(): Observable<Voto[]> {
+    return of(this.baseDatiVoti);
   }
 
 }
--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ApiServiceService } from './api-service.service';
 import { environment } from 'src/environments/environment';
 import { ApiProdService } from './api-prod.service';
 import { ApiMockService } from './api-mock.service';
 
-export function apiFactory(): ApiServiceService {
+export function apiFactory(httpClient: HttpClient): ApiServiceService {
   switch(environment.production){
-    case true: {return new ApiProdService()}
+    case true: {return new ApiProdService(httpClient)}
     default: {return new ApiMockService()}
   }
 }
