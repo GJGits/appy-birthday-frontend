73ef5450a46c1edfef68ecf1e630d0dcceb4c9f9
GJCode <golla.programmazione@gmail.com>
2022-02-08 10:50:46 +0000

    fix-#14

--- src/app/dummy-components/lista-partecipanti/lista-partecipanti.component.ts
+++ src/app/dummy-components/lista-partecipanti/lista-partecipanti.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Partecipante } from 'src/app/interfaces/partecipanti/partecipante';
+import { LogInService } from 'src/app/services/log-in.service';
 
 @Component({
   selector: 'app-lista-partecipanti',
@@ -10,9 +11,13 @@ export class ListaPartecipantiComponent implements OnInit {
 
   @Input() partecipanti: Partecipante[] | null = [];
 
-  constructor() { }
+  constructor(private loginService: LogInService) { }
 
   ngOnInit(): void {
+  }
+
+  getUserImage(userName: string) {
+    return this.loginService.getUserImagePath(userName);
   }
 
 }
